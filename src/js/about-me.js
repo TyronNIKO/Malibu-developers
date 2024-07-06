f2211fcf86fd0ebbecf139d1c5147518e5ba3e76
Volodymyr Bilohorodskyi <niko.fm@gmail.com>
2024-07-06 22:04:58 +0300

    Hero/header/aboutme/fixed logo

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,22 +1,30 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 import Swiper from 'swiper/bundle';
+import { Navigation } from 'swiper/modules';
+// import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
 
 new Accordion('.about-accordion-container');
 
-var swiper = new Swiper('.swiper', {
-  slidesPerView: 'auto', // Кількість слайдів, які видно одночасно
-  spaceBetween: 20, // Відступ між слайдами
+var swiper = new Swiper('.about-swiper', {
+  modules: [Navigation],
+  slidesPerView: 2, // Кількість слайдів, які видно одночасно
+  spaceBetween: 0, // Відступ між слайдами
   loop: true, // Безкінечна прокрутка
-  pagination: {
-    el: '.swiper-pagination', // Елемент для пагінації
-    clickable: true, // Можливість клікати на пагінаційні точки
-  },
   navigation: {
     nextEl: '.swiper-button-next', // Кнопка "Вперед"
-    prevEl: '.swiper-button-prev', // Кнопка "Назад"
   },
   keyboard: {
     enabled: true, // Взаємодія з клавіатурою
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 6,
+    },
   },
 });
--- src/js/header.js
+++ src/js/header.js
@@ -1,38 +1,41 @@
-const burgerMenu = document.querySelector('.burger-menu');
-const menuEl = document.querySelector('.header-list-menu');
-const mobMenu = document.querySelector('.mobil-menu');
-const closeBtn = document.querySelector('.mobil-menu-close-btn');
-const dropdownMenu = document.querySelector('.header-dropdown-menu-content');
-const hiddenMenu = document.querySelector('.mobil-menu-nav-list');
+function header() {
+  const burgerMenu = document.querySelector('.burger-menu');
+  const menuEl = document.querySelector('.header-list-menu');
+  const mobMenu = document.querySelector('.mobil-menu');
+  const closeBtn = document.querySelector('.mobil-menu-close-btn');
+  const dropdownMenu = document.querySelector('.header-dropdown-menu-content');
+  const hiddenMenu = document.querySelector('.mobil-menu-nav-list');
 
-// console.log(burgerMenu);
-// console.log(menuEl);
-// console.log(mobMenu);
-// console.log(closeBtn);
-// console.log(dropdownMenu);
+  // console.log(burgerMenu);
+  // console.log(menuEl);
+  // console.log(mobMenu);
+  // console.log(closeBtn);
+  // console.log(dropdownMenu);
 
-burgerMenu.addEventListener('click', onBurgerMenuClick);
-function onBurgerMenuClick() {
-  //   console.log(mobMenu.classList);
-  mobMenu.classList.add('is-open');
-}
-
-menuEl.addEventListener('click', onMenuElClick);
-function onMenuElClick() {
-  //   console.log(menuEl.classList);
-  dropdownMenu.classList.toggle('visually-hidden');
-}
-
-closeBtn.addEventListener('click', onСloseBtnClick);
-function onСloseBtnClick() {
-  mobMenu.classList.remove('is-open');
-}
-
-hiddenMenu.addEventListener('click', onMobileMenuClick);
-function onMobileMenuClick(e) {
-  if (e.target === e.currentTarget) {
-    return;
+  burgerMenu.addEventListener('click', onBurgerMenuClick);
+  function onBurgerMenuClick() {
+    //   console.log(mobMenu.classList);
+    mobMenu.classList.add('is-open');
   }
-  mobMenu.classList.remove('is-open');
-  window.location.href = e.target.dataset.source;
+
+  menuEl.addEventListener('click', onMenuElClick);
+  function onMenuElClick() {
+    //   console.log(menuEl.classList);
+    dropdownMenu.classList.toggle('visually-hidden');
+  }
+
+  closeBtn.addEventListener('click', onСloseBtnClick);
+  function onСloseBtnClick() {
+    mobMenu.classList.remove('is-open');
+  }
+
+  hiddenMenu.addEventListener('click', onMobileMenuClick);
+  function onMobileMenuClick(e) {
+    if (e.target === e.currentTarget) {
+      return;
+    }
+    mobMenu.classList.remove('is-open');
+    // window.location.href = e.target.dataset.source;
+  }
 }
+header();
