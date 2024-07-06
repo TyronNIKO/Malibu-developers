385a9dc9bf6feae679b085e755ac75dd94231baf
MakcChernenko <157323123+MakcChernenko@users.noreply.github.com>
2024-07-06 16:56:12 +0300

    begand make swiper

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,4 +1,23 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
+import Swiper from 'swiper/bundle';
 
 new Accordion('.about-accordion-container');
+
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto', // Кількість слайдів, які видно одночасно
+  spaceBetween: 20, // Відступ між слайдами
+  loop: true, // Безкінечна прокрутка
+  pagination: {
+    el: '.swiper-pagination', // Елемент для пагінації
+    clickable: true, // Можливість клікати на пагінаційні точки
+  },
+  navigation: {
+    nextEl: '.swiper-button-next', // Кнопка "Вперед"
+    prevEl: '.swiper-button-prev', // Кнопка "Назад"
+  },
+  keyboard: {
+    enabled: true, // Взаємодія з клавіатурою
+  },
+  // Додаткові опції можна додати за потреби
+});
