7077c2d3fc89943010532d8402c69ca54bcfe6be
goosedum <154931486+goosedum@users.noreply.github.com>
2024-07-06 13:59:07 +0300

    faq upd

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,4 +1,8 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
- new Accordion('.faq-list', {showMultiple: true,});
+
+new Accordion('.accordion-container', {
+    duration: 600,
+    showMultiple: false,
+  });
