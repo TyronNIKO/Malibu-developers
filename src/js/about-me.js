86c5296e9f892c3a0f5644488c0438a5e85b18fc
Volodymyr Bilohorodskyi <niko.fm@gmail.com>
2024-07-10 05:54:14 +0300

    fixed about me, swiper and projects.

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,33 +1,46 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 import Swiper from 'swiper/bundle';
-import { Navigation } from 'swiper/modules';
+import { Mousewheel, Keyboard } from 'swiper/modules';
 // import Swiper styles
-import 'swiper/css';
+// import 'swiper/css';
 // import 'swiper/css/navigation';
+function aboutMeInit() {
+    new Accordion('.about-me .accordion-container', {
+        showMultiple: true,
+        openOnInit: [0],
+    });
 
-new Accordion('.about-me .accordion-container', {
-    showMultiple: true,
-    openOnInit: [0],
-});
+    var swiper = new Swiper('.about-me .about-swiper', {
+        modules: [Mousewheel, Keyboard],
 
-// var swiper = new Swiper('.about-swiper', {
-//   modules: [Navigation],
-//   slidesPerView: 2, // Кількість слайдів, які видно одночасно
-//   spaceBetween: 0, // Відступ між слайдами
-//   loop: true, // Безкінечна прокрутка
-//   navigation: {
-//     nextEl: '.swiper-button-next', // Кнопка "Вперед"
-//   },
-//   keyboard: {
-//     enabled: true, // Взаємодія з клавіатурою
-//   },
-//   breakpoints: {
-//     768: {
-//       slidesPerView: 3,
-//     },
-//     1440: {
-//       slidesPerView: 6,
-//     },
-//   },
-// });
+        // slidesPerView: 2, // Кількість слайдів, які видно одночасно
+        slidesPerView: 'auto', // Кількість слайдів, які видно одночасно
+        spaceBetween: 0, // Відступ між слайдами
+        loop: true, // Безкінечна прокрутка
+        updateOnWindowResize: true,
+
+        mousewheel: {
+            invert: false,
+        },
+        navigation: {
+            nextEl: '.about-me .about-swiper .next',
+            preventClicks: false,
+        },
+        keyboard: {
+            enabled: true,
+            onlyInViewport: true,
+        },
+    });
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            if (event.shiftKey) {
+                swiper.slidePrev();
+            } else {
+                swiper.slideNext();
+            }
+        }
+    });
+}
+aboutMeInit();
